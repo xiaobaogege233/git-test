f954fcee8c4ebe9748ef59702ec28d423f9c86f4
xiaobao <740824080@qq.com>
2021-05-17 14:34:51 +0800

    master commit

--- src/main/java/com/xiaobao/TestGit.java
+++ src/main/java/com/xiaobao/TestGit.java
@@ -6,5 +6,6 @@ public class TestGit {
         System.out.println("hello git!");
         System.out.println("hello xiaobao!");
         System.out.println("hello yoxi!");
+        System.out.println("master test");
     }
 }
